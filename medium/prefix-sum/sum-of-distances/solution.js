b0a064b0b4083de0f97ad89b3a8cdd2ec22b2b41
Imran Ali <109467587+ftnimran@users.noreply.github.com>
2026-04-23 22:58:42 +0530

    Added Sum of Distances | medium | JavaScript

--- medium/prefix-sum/sum-of-distances/solution.js
+++ medium/prefix-sum/sum-of-distances/solution.js
@@ -4,22 +4,38 @@
  */
 var distance = function (nums) {
   const n = nums.length;
-
-  function helper(i, j, val) {
-    if (j === n) return 0;
-
-    let res = 0;
-    if (i !== j && nums[j] === val) {
-      res += Math.abs(i - j);
-    }
-
-    return res + helper(i, j + 1, val);
-  }
-
-  const res = [];
+  const map = new Map();
 
   for (let i = 0; i < n; i++) {
-    res.push(helper(i, 0, nums[i]));
+    if (!map.has(nums[i])) map.set(nums[i], []);
+    map.get(nums[i]).push(i);
+  }
+
+  const res = new Array(n).fill(0);
+
+  for (let [val, indices] of map) {
+    const m = indices.length;
+
+    let prefix = new Array(m).fill(0);
+    prefix[0] = indices[0];
+
+    for (let i = 1; i < m; i++) {
+      prefix[i] = prefix[i - 1] + indices[i];
+    }
+
+    for (let i = 0; i < m; i++) {
+      let idx = indices[i];
+
+      let leftSum = i > 0 ? prefix[i - 1] : 0;
+      let leftCount = i;
+      let leftDist = idx * leftCount - leftSum;
+
+      let rightSum = prefix[m - 1] - prefix[i];
+      let rightCount = m - i - 1;
+      let rightDist = rightSum - idx * rightCount;
+
+      res[idx] = leftDist + rightDist;
+    }
   }
 
   return res;
